dfb58ec85682f934bbcdf232494a05d942372ab2
Guilherme de Aquino <49727289+GuilhermeAquinoDB@users.noreply.github.com>
2023-10-16 20:26:39 -0300

    :sparkles: listar tarefas

--- src/main/java/br/com/guilhermeaquino/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/guilhermeaquino/todolist/task/ITaskRepository.java
@@ -3,8 +3,10 @@ package br.com.guilhermeaquino.todolist.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
 }
--- src/main/java/br/com/guilhermeaquino/todolist/task/TaskController.java
+++ src/main/java/br/com/guilhermeaquino/todolist/task/TaskController.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -37,6 +35,13 @@ public class TaskController {
         }
 
         var task = this.taskRepository.save(taskModel);
-       return ResponseEntity.status(HttpStatus.OK).body(task);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
 }
